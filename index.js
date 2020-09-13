9329bc8e6e58a15ea7ae3adce944182d8e51c411
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-12 21:50:18 -0400

    server up and running, dependencies installed, initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const helmet = require('helmet')
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+
+server.get("/", (req, res) => {
+    if(req){
+        res.status(200).send("<h1>We up</h1>")
+    } else{
+        res.status(500).json({ message: "Something is wrong" })
+    }
+})
+
+const port = process.env.PORT || 666
+server.listen(port, () => {
+    console.log(`Server up and listening on port: ${port}`)
+})
